1b5cf61880511b54e11ba55f1fe1e80040fe1872
Sissom <111325589+Sissom@users.noreply.github.com>
2023-01-24 01:44:47 +0000

    Initial commit

---
+++ main.rb
@@ -0,0 +1,14 @@
+str_1 = "center 20"
+puts str_1.center(20)
+
+str_2 = "concatenate"
+puts str_2.concat(" objects")
+
+str_3 = "delete 'this part'"
+puts str_3.delete_suffix(" 'this part'")
+
+str_4 = "DOWNCASE"
+puts str_4.downcase
+
+str_5 = "getbyte"
+puts str_5.getbyte(3)
